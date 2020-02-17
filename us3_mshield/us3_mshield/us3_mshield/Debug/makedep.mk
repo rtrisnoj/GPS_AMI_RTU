bac239004abd382e5cdd8d478404e162dc55a855
Ryan Trisnojoyo <ryan.trisnojoyo@uscubed.com>
2020-02-17 12:26:15 -0800

    first commit

---
+++ us3_mshield/us3_mshield/us3_mshield/Debug/makedep.mk
@@ -0,0 +1,12 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+EchoSensor.cpp
+
+Sketch.cpp
+
+src\libraries\MAX6675_library\max6675.cpp
+
+TempSensor.cpp
+
